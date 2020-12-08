44fad3fa2470a83d3195555dc7273c7a7296cdcd
Chip Lempke <clempke90@gmail.com>
2020-12-08 14:44:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
   new_array = []
   array = string.split
-  array.collect { |element| new_array << element.reverse}
+  array.collect {
+    |element| new_array << element.reverse
+  }
 
   new_string = new_array.join(" ")
   new_string
